5ecde0b743cbbb1920c44b2ab17eb5149999641b
TerkumaAmos <agberterkuma2020@gmail.com>
2024-10-16 13:06:08 -0700

    add

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class _DicePageState extends State<DicePage> {
             child: TextButton(
                 onPressed: () {
                   setState(() {
-                    rightDiceNumber= 6;
+                    rightDiceNumber=Random().nextInt(6);
                   });
 
                 },
